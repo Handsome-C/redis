570b946b7e75d7eeaf23f3fac7d1a96c6537de72
liyouxin <2870144621@qq.com>
2021-04-06 01:52:17 +0800

    add event module

---
+++ ae.h
@@ -0,0 +1,22 @@
+#ifndef __AE_H__
+#define __AE_H__
+
+struct aeEventLoop;
+
+typedef void aeFileProc(struct aeEventLoop *eventLoop, int fd, void *clientData, int mask);
+typedef int aeTimeProc(struct aeEventLoop *eventLoop, long long id, void *clientData);
+typedef void aeEventFinalizerProc(struct aeEventLoop *eventLoop, void *clientData);
+
+typedef struct aeFileEvent{
+    int fd;
+    int mask;
+    aeFileProc *fileProc;
+    aeEventFinalizerProc *finalizerProc;
+    void *clientData;
+    struct aeFileEvent *next;
+};
+
+typedef struct aeEventLoop {
+    long long timeEventNextId;
+
+};
---
+++ redis.c
@@ -0,0 +1,28 @@
+#define REDIS_VERSION "1.000"
+
+#include "fmacros.h"
+#include "config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#define __USE_POSIX199309
+#include <signal.h>
+#ifdef HAVE_BACKTRACE
+#include <execinfo.h>
+#include <ucontext.h>
+
+#include <sys/wait.h>
+#include <errno.h>
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <limits.h>
